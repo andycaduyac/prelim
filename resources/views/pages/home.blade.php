9f2c7ddd92f7e8502a0de7aa604c20a97b6abdf8
andycaduyac <andy.caduyac01@gmail.com>
2021-09-20 00:39:19 +0800

    prelim

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prelim</title>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+</head>
+<body>
+    <h1>Welcome</h1>
+    <h3>
+        <a href="{{url('/home')}}">Home</a> |
+        <a href="{{url('/about')}}">About Us</a> | 
+        <a href="{{url('/contact')}}">Contact Us</a>
+    </h3>
+</body>
+</html>
